e7eab3fa71c8690d23db9bbda2435ef6f1d4fcbc
Jeffrey Varner <jdv27@cornell.edu>
2024-08-22 15:08:01 -0400

    L1c
    L1c

--- examples/week-1/L1c/Include.jl
+++ examples/week-1/L1c/Include.jl
@@ -1,7 +1,7 @@
 # setup paths -
 const _ROOT = pwd();
 
-# make sure all is up to date - but only do this once
+# # make sure all is up to date - but only do this once
 using Pkg;
 if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
     Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
