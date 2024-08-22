634bd1fe3002bafc5e0191366ca7b1d7764645da
Jeffrey Varner <jdv27@cornell.edu>
2024-08-22 13:00:04 -0400

    working on L1c examples
    L1c examples

---
+++ examples/week-1/L1c/Include.jl
@@ -0,0 +1,13 @@
+# setup paths -
+const _ROOT = pwd();
+
+# make sure all is up to date - but only do this once
+using Pkg;
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load packages
+using DataFrames
+using PrettyTables
+using Unicode
