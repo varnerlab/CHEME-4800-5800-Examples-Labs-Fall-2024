8e416af65529d8f7d2b3eca9f330ac32687bbd5a
Jeffrey Varner <jdv27@cornell.edu>
2024-08-25 12:56:17 -0400

    Update Include.jl
    test

---
+++ discussion/week-2/D2d/Include.jl
@@ -0,0 +1,19 @@
+# load paths -
+const _ROOT = pwd();
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+
+
+# # make sure all is up to date - but only do this once
+using Pkg;
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load external packages -
+using Distributions
+using UnicodePlots
+using Statistics
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Factory.jl"));
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
