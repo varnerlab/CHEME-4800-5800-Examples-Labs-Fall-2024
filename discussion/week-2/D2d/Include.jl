f20eeb952b2bb57cc39ca7d67ba1046880be2ed7
Jeffrey Varner <jdv27@cornell.edu>
2024-09-06 07:32:28 -0400

    small updates
    small updates

--- discussion/week-2/D2d/Include.jl
+++ discussion/week-2/D2d/Include.jl
@@ -3,7 +3,7 @@ const _ROOT = pwd();
 const _PATH_TO_SRC = joinpath(_ROOT, "src");
 
 
-# # make sure all is up to date - but only do this once
+# make sure all is up to date - but only do this once
 using Pkg;
 if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
     Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
--- discussion/week-2/D2d/README.md
+++
@@ -1,2 +0,0 @@
-# `Lab-2d` Let's build a program to compute Fibonacci sequences
-Fill me in
