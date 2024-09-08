3dd7e567a0d842cdd10a06199563071a80d73cf0
Jeffrey Varner <jdv27@cornell.edu>
2024-09-08 11:50:29 -0400

    D3b wording changes
    D3b wording changes

--- discussion/week-3/D3b/soln/Compute.jl
+++ discussion/week-3/D3b/soln/Compute.jl
@@ -1,17 +1,14 @@
 """
     function bubble_sort(array::Array{T,1}; 
-        sounds::Union{Nothing, Dict{Int64, Tuple{Matrix{Float64}, Float32}}} = nothing) -> Array{T,1} where T <: Number
+        sounds::Union{Nothing, Dict{Int64, Tuple{Matrix{Float64}, Float32}}} = nothing)::Array{T,1} where T <: Number
 
 Sorts an `array` of `Numbers` into ascending order using the `Bubble Sort` algorithm. \\
 If the `sounds` dictionary is provided, then the sound of the array at each iteration is played. \\
 This is a `non-mutating` function.
 
 ### Arguments
-- `array::Array{T,1}`: the array to be sorted where `T` is a subtype of `Number`.
+- `array::Array{T,1}`: the array to be sorted where `T` is a subtype of `Number`.\
 - `sounds::Union{Nothing, Dict{Int64, Tuple{Matrix{Float64}, Float32}}} = nothing`: a dictionary of sounds where the keys are integers and the values are the sound matrix and the sampling frequency.
-
-### Returns
-- `Array{T,1}`: the sorted array.
 
 ### More information
 * Algorithm: https://en.wikipedia.org/wiki/Bubble_sort
@@ -35,7 +32,9 @@ function bubble_sort(array::Array{T,1};
             if arr[j] > arr[j+1]
 
                 # TODO: swap that values at j and j+1
-                throw("The bubble_sort function is not implemented yet.");
+                tmp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = tmp
 
             end
         end
@@ -47,7 +46,7 @@ end
 
 """
     function bubble_sort!(array::Array{T,1}; 
-        sounds::Union{Nothing, Dict{Int64, Tuple{Matrix{Float64}, Float32}}} = nothing)::Nothing where T <: Number
+        sounds::Union{Nothing, Dict{Int64, Tuple{Matrix{Float64}, Float32}}} = nothing)::Array{T,1} where T <: Number
 
 Sorts an `array` of `Numbers` into ascending order using the `Bubble Sort` algorithm. \\
 If the `sounds` dictionary is provided, then the sound of the array at each iteration is played. \\
@@ -57,14 +56,11 @@ This is a `mutating` function.
 - `array::Array{T,1}`: the array to be sorted where `T` is a subtype of `Number`.
 - `sounds::Union{Nothing, Dict{Int64, Tuple{Matrix{Float64}, Float32}}} = nothing`: a dictionary of sounds where the keys are integers and the values are the sound matrix and the sampling frequency.
 
-### Returns
-- `Nothing`. This function mutates the input array, and sorts it in place.
-
 ### More information
 * Algorithm: https://en.wikipedia.org/wiki/Bubble_sort
 """
-function bubble_sort!(array::Array{T,1}; 
-    sounds::Union{Nothing, Dict{Int64, Tuple{Matrix{Float64}, Float32}}} = nothing)::Nothing where T <: Number
+function bubble_sort_2(array::Array{T,1}; 
+    sounds::Union{Nothing, Dict{Int64, Tuple{Matrix{Float64}, Float32}}} = nothing)::Array{T,1} where T <: Number
 
     # initialize -
     arr = array # do nothing, just use the array directly
@@ -82,8 +78,13 @@ function bubble_sort!(array::Array{T,1};
             if arr[j] > arr[j+1]
 
                 # TODO: swap that values at j and j+1
-                throw("The bubble_sort! function is not implemented yet.");
+                tmp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = tmp
             end
         end
     end
+
+    # return sorted array -
+    return arr
 end
--- discussion/week-3/D3b/testme_mutating_bubble_sort.jl
+++ discussion/week-3/D3b/testme_mutating_bubble_sort.jl
@@ -4,7 +4,7 @@ include("Include.jl");
 # let's create a random array of numbers, and then sort it using the mutating version of he bubble sort algorithm -
 length_of_array = 25;
 random_input_array = rand(1:number_of_samples, length_of_array);
-sorted_output_array = bubble_sort_2(random_input_array; sounds = nothing);
+sorted_output_array = bubble_sort!(random_input_array; sounds = nothing);
 
 # Let's do an @assert to check if the sorted_output_array is indeed sorted -
 # This test makes use of the sorting algorithm provided by Julia: https://docs.julialang.org/en/v1/base/sort/#Base.sort
