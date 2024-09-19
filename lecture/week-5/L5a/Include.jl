11bc0836924de385bb1d592163ca461d86d288b3
Jeffrey Varner <jdv27@cornell.edu>
2024-09-19 05:19:23 -0400

    examples
    examples

--- lecture/week-5/L5a/Include.jl
+++ lecture/week-5/L5a/Include.jl
@@ -3,8 +3,14 @@ const _ROOT = @__DIR__
 const _PATH_TO_SRC = joinpath(_ROOT, "src")
 
 # if we are missing any packages, install them -
-using Pkg
-Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+# using Pkg
+# Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+
+# if we are missing any packages, install them -
+using Pkg;
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
 
 # load external package -
 using BenchmarkTools
