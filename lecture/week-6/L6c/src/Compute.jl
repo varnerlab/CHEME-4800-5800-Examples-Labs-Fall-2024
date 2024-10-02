25e8fb8dbabb30d99ec5978bd0ee8f6e972c30b2
Jeffrey Varner <jdv27@cornell.edu>
2024-10-02 07:37:44 -0400

    updates to comments
    update to commennts

--- lecture/week-6/L6c/src/Compute.jl
+++ lecture/week-6/L6c/src/Compute.jl
@@ -30,11 +30,34 @@ end
 
 """
     function children(graph::T, node::MyGraphNodeModel) -> Set{Int64} where T <: MyAbstractGraphModel
+
+This function returns the children of a node in a graph model.
+
+### Arguments
+- `graph::T`: the graph model to search. This is a subtype of `MyAbstractGraphModel`.
+- `node::MyGraphNodeModel`: the node to search for children.
+
+### Returns
+- a set of children node ids.
 """
 function children(graph::T, node::MyGraphNodeModel)::Set{Int64} where T <: MyAbstractGraphModel
     return graph.children[node.id];
 end
 
+
+"""
+    function weight(graph::T, source::Int64, target::Int64) -> Float64 where T <: MyAbstractGraphModel
+
+This function returns the weight of the edge between two nodes in a graph model.
+
+### Arguments
+- `graph::T`: the graph model to search. This is a subtype of `MyAbstractGraphModel`.
+- `source::Int64`: the source node id.
+- `target::Int64`: the target node id.
+
+### Returns
+- the weight of the edge between the source and target nodes.
+"""
 function weight(graph::T, source::Int64, target::Int64)::Float64 where T <: MyAbstractGraphModel
     return graph.edges[(source, target)];
 end
--- lecture/week-6/L6c/src/Search.jl
+++ lecture/week-6/L6c/src/Search.jl
@@ -1,29 +1,42 @@
 
+"""
+    function _search(graph::T, start::MyGraphNodeModel, algorithm::DikjstraAlgorithm) where T <: MyAbstractGraphModel
+
+This function performs a single source shortest path calculation on a graph model using the Dikjstra algorithm.
+
+### Arguments
+- `graph::T`: the graph model to search. This is a subtype of `MyAbstractGraphModel`.
+- `start::MyGraphNodeModel`: the node to start the search from.
+- `algorithm::DikjstraAlgorithm`: the algorithm to use for the search {DikjstraAlgorithm | BellmanFordAlgorithm}.
+
+### Returns
+- a tuple of two dictionaries: the first dictionary contains the distances from the starting node to all other nodes, and the second dictionary contains the previous node in the shortest path from the starting node to all other nodes.
+"""
 function _search(graph::T, start::MyGraphNodeModel, algorithm::DikjstraAlgorithm) where T <: MyAbstractGraphModel
     
     # initialize -
     distances = Dict{Int64, Float64}();
     previous = Dict{Int64, Union{Nothing,Int64}}();
-    queue = PriorityQueue{Int64, Float64}();
+    queue = PriorityQueue{Int64, Float64}(); # exported from DataStructures.jl
 
     # set distances and previous -
-    distances[start.id] = 0.0;
-    for (k, _) ∈ graph.nodes
+    distances[start.id] = 0.0; # distance from start to start is zero
+    for (k, _) ∈ graph.nodes # what is this?
         if k != start.id
             distances[k] = Inf;
             previous[k] = nothing;
         end
-        enqueue!(queue, k, distances[k]);
+        enqueue!(queue, k, distances[k]); # add nodes to the queue
     end
 
     # main loop -
-    while !isempty(queue)
+    while !isempty(queue) # process nodes in the queue until it is empty (!isempty(queue) is the same as isempty(queue) == false)
         u = dequeue!(queue);
         mychildren = children(graph, graph.nodes[u]);
 
-        for w ∈ mychildren
-            alt = distances[u] + weight(graph, u, w);
-            if alt < distances[w]
+        for w ∈ mychildren # iterate over the children set of the current node
+            alt = distances[u] + weight(graph, u, w); # distance to u so far + weight of edge from u to w
+            if alt < distances[w] # Wow! the distance to w is less than the current best distance to w
                 distances[w] = alt;
                 previous[w] = u;
                 queue[w] = alt;
@@ -31,6 +44,7 @@ function _search(graph::T, start::MyGraphNodeModel, algorithm::DikjstraAlgorithm
         end
     end
 
+    # return -
     return distances, previous;
 end
 
