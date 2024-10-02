44fe804066bba422f00730fa67f480b6828bc4ac
Jeffrey Varner <jdv27@cornell.edu>
2024-10-02 07:42:35 -0400

    undirected
    undirected

--- discussion/week-6/D6b/runme_task_3.jl
+++ discussion/week-6/D6b/runme_task_3.jl
@@ -1,7 +1,7 @@
 include("Include.jl")
 
 # load up the directed graph -
-adj_matrix = readdlm(joinpath(_PATH_TO_DATA, "Testgraph-directed.dat"), Int64, comment_char='#', comments=true);
+adj_matrix = readdlm(joinpath(_PATH_TO_DATA, "Testgraph-undirected.dat"), Int64, comment_char='#', comments=true);
 dag = build(MySimpleUndirectedGraphModel, adj_matrix);
 
 # run DFS in the directed graph -
