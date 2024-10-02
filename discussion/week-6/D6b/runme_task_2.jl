1aedc64b6fd83663b747f932ed69a3278b217bab
Jeffrey Varner <jdv27@cornell.edu>
2024-10-02 07:39:07 -0400

    Update runme_task_2.jl
    small change to the runme

--- discussion/week-6/D6b/runme_task_2.jl
+++ discussion/week-6/D6b/runme_task_2.jl
@@ -3,6 +3,7 @@ include("Include.jl")
 # load up the directed graph -
 adj_matrix = readdlm(joinpath(_PATH_TO_DATA, "Testgraph-directed.dat"), Int64, comment_char='#', comments=true);
 dag = build(MySimpleUndirectedGraphModel, adj_matrix);
+visited = Set{Int64}();
 
 # run DFS in the directed graph -
-DFS(dag, dag.nodes[0], Set{Int64}());
+DFS(dag, dag.nodes[0], visited);
