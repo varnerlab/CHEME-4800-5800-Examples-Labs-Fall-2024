70c77b3e65017f79f4fb7031a40b9c28a6cf79d2
Jeffrey Varner <jdv27@cornell.edu>
2024-11-04 07:35:56 -0500

    Small changes to lecture examples L11a
    L11a

--- lecture/week-11/L11a/Include.jl
+++ lecture/week-11/L11a/Include.jl
@@ -4,7 +4,9 @@ const _PATH_TO_SRC = joinpath(_ROOT, "src");
 
 # check: do we need to download any packages?
 using Pkg
-Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
 
 # load the required packages -
 using Distributions
