20afa40f4473f7f1ed1c41de2ed2baec7f47a878
Jeffrey Varner <jdv27@cornell.edu>
2024-11-07 14:19:38 -0500

    hacking
    ok

---
+++ discussion/week-11/D11d/Include.jl
@@ -0,0 +1,22 @@
+# setup paths -
+const _ROOT = pwd();
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+
+# download external packages
+using Pkg;
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load external packages -
+using VLDecisionsPackage
+using LinearAlgebra
+using Statistics
+using Plots
+using Colors
+using Distributions
+using JLD2
+using FileIO
+
+# include my codes -
+# ...
