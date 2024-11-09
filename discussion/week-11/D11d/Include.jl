e9ee666787256320d3479e17768222dca7f00b82
Jeffrey Varner <jdv27@cornell.edu>
2024-11-09 06:40:28 -0500

    Update Include.jl
    added VL

--- discussion/week-11/D11d/Include.jl
+++ discussion/week-11/D11d/Include.jl
@@ -5,7 +5,7 @@ const _PATH_TO_SRC = joinpath(_ROOT, "src");
 # download external packages
 using Pkg;
 if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
-    
+    Pkg.add(path="https://github.com/varnerlab/VLDecisionsPackage.jl.git")
     Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
 end
 
